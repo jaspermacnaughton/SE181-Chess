025bf18cb5df2bf43ab1270019b3bd6bc56fb3cf
Damien Prieur <dap789@live.com>
2020-03-06 15:15:00 -0500

    Server: Added endpoints, probably need to fix them once we can test. Added default board for chess game as static method

--- server/src/chess.js
+++ server/src/chess.js
@@ -113,6 +113,21 @@ class GameState {
     return empty_locations;
   }
 
+  static default_board(){
+    var black = Players.BLACK.COLOR;
+    var white = Players.BLACK.COLOR;
+    return [
+    [new Rook(black,(0,0)), new Knight(black,(1,0)), new Bishop(black,(2,0)), new Queen(black,(3,0)), new King(black,(4,0)), new Bishop(black,(5,0)), new Knight(black,(6,0)), new Rook(black,(7,0))],
+    [new Pawn(black,(0,1)), new Pawn(black,(1,1))  , new Pawn(black,(2,1))  , new Pawn(black,(3,1)) , new Pawn(black,(4,1)), new Pawn(black,(5,1))  , new Pawn(black,(6,1))  , new Pawn(black,(7,1))],
+    [null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null],
+    [new Pawn(white,(0,6)), new Pawn(white,(1,6))  , new Pawn(white,(2,6))  , new Pawn(white,(3,6)) , new Pawn(white,(4,6)), new Pawn(white,(5,6))  , new Pawn(white,(6,6))  , new Pawn(white,(7,6))],
+    [new Rook(white,(0,7)), new Knight(white,(1,7)), new Bishop(white,(2,7)), new Queen(white,(3,7)), new King(white,(4,7)), new Bishop(white,(5,7)), new Knight(white,(6,7)), new Rook(white,(7,7))],
+    ];
+  }
+
 }
 class Piece {
 
@@ -275,4 +290,9 @@ class King extends Piece {
   }
 }
 
-
+module.exports = {
+    GameState : GameState,
+    Players : Players,
+    MoveStatus : MoveStatus,
+    Location : Location
+}
--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,213 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const port = 8080;
 
+var chess = require(__dirname + '/chess.js');
+
+var player_ips = [];
+var inGame = false;
+var winner = null;
+
+var game = new chess.GameState(chess.GameState.default_board(), player_ips, chess.Players.WHITE.COLOR);
+
+// Returns index of player in player_ips or -1 if it doesn't exist
+function get_player_id(ip){
+    return player_ips.indexOf(id);
+}
+
+function valid_player(id){
+    return id == 0 || id == 1;
+}
+
 app.get('/', (req,res) => {
+    // Check if it's a new connection, if it is then add them
+    // to the player_ips list if there are not already 2 players
+    var player = get_player_id(req.connection.remoteAddress);
+    if(player == -1){
+        if(player_ips.length < 2){
+            player_ips.push(req.connection.remoteAddress);
+        }else{
+            res.write("Already have two players");
+            res.end();
+            return;
+        }
+    }
+    // Send all client stuff here
     res.write("<h1>Chess server</h1>");
     res.end();
+});
+
+app.get('/send_move', (req,res) => {
+    var player = get_player_id(req.connection.remoteAddress);
+    if(!valid_player(player)){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Not currently a valid player"
+        });
+        res.end();
+        return;
+    }
+    
+    var start_loc = req.body.start;
+    var end_loc = req.body.end;
+    var promotion = req.body.promotion;
+
+    if(start_loc.row == null || start_loc.col == null){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Invalid start location"
+        });
+        res.end();
+        return;
+    }
+    start_loc = new chess.Location(start_loc.row, start_loc.col);
+
+    if(end_loc.row == null || end_loc.col == null){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Invalid end location"
+        });
+        res.end();
+        return;
+    }
+    end_loc = new chess.Location(end_loc.row, end_loc.col);
+
+    if(promotion == undefined){
+        promotion = null;
+    }
+
+    var stat = game.play_move(start_loc, end_loc, promotion);
+    // update global state for sync with other player
+    if(stat == chess.MoveStatus.BLACK_WIN || stat == chess.MoveStatus.WHITE_WIN || stat == chess.MoveStatus.STALE_MATE){
+        inGame = false;
+        winner = stat;
+    }
+
+    res.json({
+        "status" : stat
+    });
+
+});
+
+app.get('/sync', (req,res) => {
+    var player = get_player_id(req.connection.remoteAddress);
+    if(!valid_player(player)){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Not currently a valid player"
+        });
+        res.end();
+        return;
+    }
+
+    res.json({
+        "GameState" : game,
+        "inGame" : inGame,
+        "game_status" : winner
+    });
+});
+
+app.get('/get_moves', (req,res) => {
+    var player = get_player_id(req.connection.remoteAddress);
+    if(!valid_player(player)){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Not currently a valid player"
+        });
+        res.end();
+        return;
+    }
+
+    var piece_loc = req.body.piece;
+    if(piece_loc.row == null || piece_loc.col == null){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Invalid location"
+        });
+        res.end();
+        return;
+    }
+
+    var piece = game.get_piece_on_board(new chess.Location(loc.row, loc.col));
+    var moves = piece.get_moves(game);
+
+    res.json({
+        "status" : chess.MoveStatus.Success,
+        "moves" : moves
+    });
+
+});
+
+app.get('/restart', (req,res) => {
+    game = new chess.GameState(chess.GameState.default_board(), player_ips, chess.Players.WHITE.COLOR);
+    inGame = true;
+    res.json({"status" : chess.MoveStatus.SUCCESS});
+    res.send();
+});
+
+// This is questionable if a color request is sent midgame or something....
+app.get('/color', (req,res) => {
+    var player = get_player_id(req.connection.remoteAddress);
+    if(!valid_player(player)){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Not currently a valid player"
+        });
+        res.end();
+        return;
+    }
+
+    var color;
+    switch(req.body.color){
+        case "white":
+            if(player != chess.Players.WHITE.COLOR){
+                color = player_ips[0];
+                player_ips[0] = player_ips[1];
+                player_ips[1] = color;
+            }
+            color = "white";
+            break;
+        case "black":
+            if(player != chess.Players.BLACK.COLOR){
+                color = player_ips[0];
+                player_ips[0] = player_ips[1];
+                player_ips[1] = color;
+            }
+            color = "black";
+            break;
+        default:
+            color = (player) ? "white" : "black";
+            break;
+    }
+
+    res.json({
+        "status" : chess.MoveStatus.Success,
+        "color" : color
+    });
+});
+
+app.get('/resign', (req,res) => {
+    var player = get_player_id(req.connection.remoteAddress);
+    if(!valid_player(player)){
+        res.json({
+            "status" : chess.MoveStatus.INVALID,
+            "msg" : "Not currently a valid player",
+        });
+        return;
+    }
+
+    game = new chess.GameState(chess.default_board(), player_ips, chess.Players.WHITE.COLOR);
+
+    if(player == chess.Players.WHITE.COLOR){
+        winner = chess.Players.BLACK.COLOR;
+        res.json({
+            "status" : chess.MoveStatus.BLACK_WIN
+        });
+    }else{
+        winner = chess.Players.WHITE.COLOR;
+        res.json({
+            "status" : chess.MoveStatus.WHITE_WIN
+        });
+    };
 });
 
 app.listen(port, () => {
